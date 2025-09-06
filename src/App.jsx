6bd971d15ab65792a1050b789c6f5a4ab330bf5c
benja3086 <benjaminquinteros785@gmail.com>
2025-09-06 00:43:48 -0300

    Subiendo mi página inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Radio en Vivo</h1>
+      <iframe
+        src="https://streaming.radiosenlinea.com.ar/cp/widgets/player/single/?p=8150"
+        style={{ width: "100%", border: "none", height: "150px" }}
+        title="Radio en Vivo"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
